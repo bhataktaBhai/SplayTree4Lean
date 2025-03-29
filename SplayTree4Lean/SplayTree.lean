edf5451d78d2efbc522c1632e1cf26ae2991d529
Aatman <aatmansupkar@gmail.com>
2025-03-29 16:43:01 +0530

    Implemented join and split, and then implemented the insert and delete functions. Added constructors from and to a list.
    Need to add more goals and work on the
    construction monad.

--- SplayTree4Lean/SplayTree.lean
+++ SplayTree4Lean/SplayTree.lean
@@ -4,50 +4,86 @@ import SplayTree4Lean.LinOrdNat
 inductive SplayTree (α : Type)
   | empty : SplayTree α
   | node (val : α) (left right : SplayTree α) : SplayTree α
-deriving Repr
+
+namespace SplayTree
 
 def rotateRight {α : Type} : SplayTree α → SplayTree α
-  | SplayTree.node x (SplayTree.node y A B) C => SplayTree.node y A (SplayTree.node x B C)
+  | node x (node y A B) C => node y A (node x B C)
   | t => t
 
 def rotateLeft {α : Type} : SplayTree α → SplayTree α
-  | SplayTree.node x A (SplayTree.node y B C) => SplayTree.node y (SplayTree.node x A B) C
+  | node x A (node y B C) => node y (node x A B) C
   | t => t
 
 def splay {α : Type} [LinearOrder α] (x : α) : SplayTree α → SplayTree α
-  | SplayTree.empty => SplayTree.empty
-  | SplayTree.node y A B =>
+  | empty => empty
+  | node y A B =>
       if x < y then
         match A with
-        | SplayTree.node z _ _ =>
-            if x < z then rotateRight (SplayTree.node y (rotateRight A) B)
-            else rotateRight (SplayTree.node y (rotateLeft A) B)
-        | _ => SplayTree.node y A B
+        | node z _ _ =>
+            if x < z then rotateRight (node y (rotateRight A) B)
+            else rotateRight (node y (rotateLeft A) B)
+        | _ => node y A B
       else if x > y then
         match B with
-        | SplayTree.node z _ _ =>
-            if x > z then rotateLeft (SplayTree.node y A (rotateLeft B)) -- Zig-Zig
-            else rotateLeft (SplayTree.node y A (rotateRight B)) -- Zig-Zag
-        | _ => SplayTree.node y A B
-      else SplayTree.node y A B
-
-def insert {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree α :=
-  let t' := splay x t
-  match t' with
-  | SplayTree.empty => SplayTree.node x SplayTree.empty SplayTree.empty
-  | SplayTree.node y A B =>
-      if x < y then SplayTree.node x A (SplayTree.node y SplayTree.empty B)
-      else if x > y then SplayTree.node x (SplayTree.node y A SplayTree.empty) B
-      else t'
+        | node z _ _ =>
+            if x > z then rotateLeft (node y A (rotateLeft B))
+            else rotateLeft (node y A (rotateRight B))
+        | _ => node y A B
+      else node y A B
 
 def find {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree α :=
   splay x t
 
-def exampleTree : SplayTree Nat :=
+def split {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree α × SplayTree α :=
+  let t' := splay x t
+  match t' with
+  | empty => (empty, empty)
+  | node y A B =>
+      if x < y then (A, node y empty B)
+      else if x > y then (node y A empty, B)
+      else (A, B)
+
+def join {α : Type} [LinearOrder α] (A B : SplayTree α) : SplayTree α :=
+  match A, B with
+  | empty, _ => B
+  | _, empty => A
+  | node y A1 A2, _ => node y A1 (join A2 B)
+
+def insert {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree α :=
+  let (L, R) := split x t
+  node x L R
+
+def delete {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree α :=
+  let t' := splay x t
+  match t' with
+  | empty => empty
+  | node y A B => if y = x then join A B else t'
+
+def fromList {α : Type} [LinearOrder α] (L : List α) : SplayTree α :=
+  L.foldl (fun t x => insert x t) empty
+
+def toList {α : Type} : SplayTree α → List α
+  | empty => []
+  | node x l r => toList l ++ [x] ++ toList r
+
+end SplayTree
+
+section demo
+open SplayTree
+
+def exampleTree1 : SplayTree Nat :=
   insert 10 (insert 20 (insert 30 (insert 25 (insert 5 (insert (15 : ℕ) SplayTree.empty)))))
 
-#eval exampleTree  -- Check the tree structure
+def L : List Nat := [5, 3, 8, 1, 4, 7, 9]
 
-#eval find 15 exampleTree  -- Splay 15 to root
+def exampleTree2 : SplayTree Nat :=
+  fromList L
 
-#eval find 5 exampleTree   -- Splay 5 to root
+#eval exampleTree1
+#eval exampleTree2
+#eval find 15 exampleTree1
+#eval find 5 exampleTree1
+#eval delete 5 exampleTree1
+
+end demo

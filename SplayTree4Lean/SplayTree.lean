3d965a6df01e6b2755aab078d079866cfbb943e7
bhataktaBhai <naman.105.mishra@gmail.com>
2025-04-01 23:14:28 +0530

    Fixed the `splay` function

--- SplayTree4Lean/SplayTree.lean
+++ SplayTree4Lean/SplayTree.lean
@@ -15,22 +15,72 @@ def rotateRightChild {α : Type} : SplayTree α → SplayTree α
   | node x A (node y B C) => node y (node x A B) C
   | t => t
 
-def splay {α : Type} [LinearOrder α] (x : α) : SplayTree α → SplayTree α
+/--
+Inductive type to keep track of where a particular value is present in a tree,
+in the first two levels: at the `root`, the `left` child of the root,
+the `right` child of the root, or `idk` because I do not know what to name this
+case.
+-/
+inductive Location
+  | root | left | right | idk
+
+/-- Returns the `Location` of the supplied value in the supplied tree. -/
+def locationOf {α : Type} [LinearOrder α] (x : α) : SplayTree α → Location
+  | nil => .idk
+  | node y yL yR =>
+    if x = y then .root
+    else if x < y then
+      match yL with
+      | nil => .idk
+      | node yl _ _ =>
+          if x = yl then .left
+          else .idk
+    else
+      match yR with
+      | nil => .idk
+      | node yr _ _ =>
+          if x = yr then .right
+          else .idk
+
+/--
+Looks for a value `x` in a `SplayTree`.
+If found, splays the tree at that node, executing zig-zig and zig-zag steps
+but *not* a zig step.
+That is, if `x` ends up as a child of the root, a final rotation to bring it to
+the root is *not* performed.
+This is necessary for recursion to work in the `splay` function.
+-/
+def splayButOne {α : Type} [LinearOrder α] (x : α) : SplayTree α → SplayTree α
   | nil => nil
-  | node y A B =>
-      if x < y then
-        match A with
-        | node z _ _ =>
-            if x < z then rotateLeftChild (node y (rotateLeftChild A) B)
-            else rotateLeftChild (node y (rotateRightChild A) B)
-        | _ => node y A B
-      else if x > y then
-        match B with
-        | node z _ _ =>
-            if x > z then rotateRightChild (node y A (rotateRightChild B))
-            else rotateRightChild (node y A (rotateLeftChild B))
-        | _ => node y A B
-      else node y A B
+  | node y yL yR =>
+      if x = y then
+        node y yL yR
+      else if x < y then
+        let yL' := splayButOne x yL
+        match locationOf x yL' with
+        | .root => node y yL' yR
+        | .left => node y yL' yR |> rotateLeftChild |> rotateLeftChild -- zig-zig
+        | .right => node y (rotateRightChild yL') yR |> rotateLeftChild -- zig-zag
+        | .idk => node y yL' yR
+      else
+        let yR' := splayButOne x yR
+        match locationOf x yR' with
+        | .root => node y yL yR'
+        | .right => node y yL yR' |> rotateRightChild |> rotateRightChild -- zig-zig
+        | .left => node y yL (rotateLeftChild yR') |> rotateRightChild -- zig-zag
+        | .idk => node y yL yR'
+
+/--
+Looks for a value `x` in a `SplayTree`.
+If found, splays the tree at that node.
+-/
+def splay {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree α :=
+  let t' := splayButOne x t
+  match locationOf x t' with
+  | .root => t'
+  | .left => rotateLeftChild t'
+  | .right => rotateRightChild t'
+  | .idk => t'
 
 def find {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree α :=
   splay x t

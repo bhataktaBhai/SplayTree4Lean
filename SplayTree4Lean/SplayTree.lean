4a1727d8977881f9a8ef6ffe0bf850997aedf134
bhataktaBhai <naman.105.mishra@gmail.com>
2025-04-01 11:20:12 +0530

    Renaming for consistency and/or clarity

--- SplayTree4Lean/SplayTree.lean
+++ SplayTree4Lean/SplayTree.lean
@@ -2,33 +2,33 @@ import Mathlib.Order.Basic -- for LinearOrder
 import Mathlib.Data.Nat.Basic -- for LinearOrder Nat
 
 inductive SplayTree (α : Type)
-  | empty : SplayTree α
+  | nil : SplayTree α
   | node (val : α) (left right : SplayTree α) : SplayTree α
 
 namespace SplayTree
 
-def rotateRight {α : Type} : SplayTree α → SplayTree α
+def rotateLeftChild {α : Type} : SplayTree α → SplayTree α
   | node x (node y A B) C => node y A (node x B C)
   | t => t
 
-def rotateLeft {α : Type} : SplayTree α → SplayTree α
+def rotateRightChild {α : Type} : SplayTree α → SplayTree α
   | node x A (node y B C) => node y (node x A B) C
   | t => t
 
 def splay {α : Type} [LinearOrder α] (x : α) : SplayTree α → SplayTree α
-  | empty => empty
+  | nil => nil
   | node y A B =>
       if x < y then
         match A with
         | node z _ _ =>
-            if x < z then rotateRight (node y (rotateRight A) B)
-            else rotateRight (node y (rotateLeft A) B)
+            if x < z then rotateLeftChild (node y (rotateLeftChild A) B)
+            else rotateLeftChild (node y (rotateRightChild A) B)
         | _ => node y A B
       else if x > y then
         match B with
         | node z _ _ =>
-            if x > z then rotateLeft (node y A (rotateLeft B))
-            else rotateLeft (node y A (rotateRight B))
+            if x > z then rotateRightChild (node y A (rotateRightChild B))
+            else rotateRightChild (node y A (rotateLeftChild B))
         | _ => node y A B
       else node y A B
 
@@ -38,16 +38,16 @@ def find {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree α
 def split {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree α × SplayTree α :=
   let t' := splay x t
   match t' with
-  | empty => (empty, empty)
+  | nil => (nil, nil)
   | node y A B =>
-      if x < y then (A, node y empty B)
-      else if x > y then (node y A empty, B)
+      if x < y then (A, node y nil B)
+      else if x > y then (node y A nil, B)
       else (A, B)
 
 def join {α : Type} [LinearOrder α] (A B : SplayTree α) : SplayTree α :=
   match A, B with
-  | empty, _ => B
-  | _, empty => A
+  | nil, _ => B
+  | _, nil => A
   | node y A1 A2, _ => node y A1 (join A2 B)
 
 def insert {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree α :=
@@ -57,14 +57,14 @@ def insert {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree 
 def delete {α : Type} [LinearOrder α] (x : α) (t : SplayTree α) : SplayTree α :=
   let t' := splay x t
   match t' with
-  | empty => empty
+  | nil => nil
   | node y A B => if y = x then join A B else t'
 
 def fromList {α : Type} [LinearOrder α] (L : List α) : SplayTree α :=
-  L.foldl (fun t x => insert x t) empty
+  L.foldl (fun t x => insert x t) nil
 
 def toList {α : Type} : SplayTree α → List α
-  | empty => []
+  | nil => []
   | node x l r => toList l ++ [x] ++ toList r
 
 end SplayTree
@@ -73,7 +73,7 @@ section demo
 open SplayTree
 
 def exampleTree1 : SplayTree Nat :=
-  insert 10 (insert 20 (insert 30 (insert 25 (insert 5 (insert (15 : ℕ) SplayTree.empty)))))
+  insert 10 (insert 20 (insert 30 (insert 25 (insert 5 (insert (15 : ℕ) SplayTree.nil)))))
 
 def L : List Nat := [5, 3, 8, 1, 4, 7, 9]
 
